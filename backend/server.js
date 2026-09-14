90a36b342268c1b276260b0e966d4a48324325a6
FaturRosek <faturrosek0403@gmail.com>
2026-09-14 13:22:37 +0700

    Feat: trust proxy

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ const socketService = require("./src/services/socketService");
 const { globalLimiter } = require("./src/middleware/rateLimiter");
 
 const app = express();
+app.set("trust proxy", 1);
 const server = http.createServer(app);
 const PORT = process.env.PORT || 5000;
 
